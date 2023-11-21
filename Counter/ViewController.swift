3ed5d95ea0c1c183709b1f37aadc07c8def1d9ca
Ilya Shirokov <ilya-cra-sh@yandex.ru>
2023-11-21 20:52:26 +0300

    Добавил lable с выводом счетчика. Кнопик + и - и кнопку reset для обнуления

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var countLable: UILabel!
+    @IBOutlet weak var plusCountButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var minesCountButton: UIButton!
+    
+    var count : Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        countLable.text = "0"
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func tapDidPlusCount(_ sender: Any) {
+        count += 1
+        countLable.text = "\(count)"
+    }
 
+    
+    @IBAction func tapDidMinesCount(_ sender: Any) {
+        count -= 1
+        countLable.text = "\(count)"
+        
+        if count < 0 {
+            count = 0
+            countLable.text = "\(count)"
+            
+        }
+    }
+    
+    @IBAction func tapDidResetTo0(_ sender: Any) {
+        count = 0
+        countLable.text = "\(count)"
+    }
+    
+    
 }
 
